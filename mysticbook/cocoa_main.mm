13e418507ac7e75a94d6ee9e1f2e9fd0285d31db
Andrew Shinjo <shinjo1233@gmail.com>
2025-11-01 13:05:22 -0700

    Mysticbook: open a window in cocoa.

--- mysticbook/cocoa_main.mm
+++ mysticbook/cocoa_main.mm
@@ -1,8 +1,39 @@
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@property (strong) NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
+}
+
+@end
 
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
     NSLog(@"Mysticbook [cocoa]");
+
+    NSApplication *app = [NSApplication sharedApplication];
+
+    NSWindow *window = [
+      [NSWindow alloc]
+      initWithContentRect:NSMakeRect(0, 0, 800, 600)
+      styleMask:NSWindowStyleMaskTitled |
+        NSWindowStyleMaskClosable |
+        NSWindowStyleMaskResizable
+      backing:NSBackingStoreBuffered
+      defer:NO
+    ];
+
+    [window setTitle:@"Mysticbook [cocoa]"];
+
+    [app setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [app activateIgnoringOtherApps:YES];
+    [window makeKeyAndOrderFront:nil];
+    [app run];
   }
   return 0;
 }
