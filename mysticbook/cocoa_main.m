e290ed2f2a16a4b3e18685926a973375e5b9a893
Andrew Shinjo <shinjo1233@gmail.com>
2025-11-01 22:52:49 -0700

    Initial cocoa wrapper

--- mysticbook/cocoa_main.m
+++ mysticbook/cocoa_main.m
@@ -8,12 +8,22 @@ int main(int argc, const char *argv[]) {
 
     NSApplication *app = [[[CocoaAppBuilder alloc] init] build];
 
+<<<<<<< HEAD
     NSWindow *window =[[[[[
       CocoaWindowBuilder alloc] 
       init] 
       withFrame:NSMakeRect(0,0,800,600)]
       withTitle:@"Mysticbook [cocoa]"]
       build];
+=======
+    NSWindow *window =
+        [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
+                                    styleMask:NSWindowStyleMaskTitled |
+                                              NSWindowStyleMaskClosable |
+                                              NSWindowStyleMaskResizable
+                                      backing:NSBackingStoreBuffered
+                                        defer:NO];
+>>>>>>> cd2f826 (Initial cocoa wrapper)
 
     [window makeKeyAndOrderFront:nil];
     [app run];
