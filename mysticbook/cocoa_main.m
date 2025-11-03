34fa49195da0e2485b3fef509bd86818aa916442
Andrew Shinjo <shinjo1233@gmail.com>
2025-11-02 20:23:32 -0800

    Added metal layer

--- mysticbook/cocoa_main.m
+++ mysticbook/cocoa_main.m
@@ -1,6 +1,9 @@
+#import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <QuartzCore/CAMetalLayer.h>
+
 #include "CocoaAppBuilder.h"
 #import "CocoaWindowBuilder.h"
-#import <Cocoa/Cocoa.h>
 
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
@@ -14,6 +17,40 @@ int main(int argc, const char *argv[]) {
       withFrame:NSMakeRect(0,0,800,600)]
       withTitle:@"Mysticbook [cocoa]"]
       build];
+
+    NSView *contentView = window.contentView;
+    contentView.wantsLayer = true;
+
+    // Setup metal.
+    CAMetalLayer *metalLayer = [CAMetalLayer layer];
+    metalLayer.device = MTLCreateSystemDefaultDevice();
+    metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    metalLayer.frame = contentView.bounds;
+    metalLayer.contentsScale = [NSScreen mainScreen].backingScaleFactor;
+    [contentView.layer addSublayer:metalLayer];
+
+    // Draw something in metal.
+    id<MTLDevice> device = metalLayer.device;
+    id<MTLCommandQueue> commandQueue = [device newCommandQueue];
+    id<CAMetalDrawable> drawable = [metalLayer nextDrawable];
+
+    if(drawable) {
+      MTLRenderPassDescriptor *renderPassDescriptor 
+        = [MTLRenderPassDescriptor renderPassDescriptor];
+      renderPassDescriptor.colorAttachments[0].texture = drawable.texture;
+      renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+      renderPassDescriptor.colorAttachments[0].clearColor 
+        = MTLClearColorMake(0.2, 0.4, 0.6, 1.0);
+      
+      id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
+      id<MTLRenderCommandEncoder> renderCommandEncoder = [
+        commandBuffer 
+        renderCommandEncoderWithDescriptor:renderPassDescriptor
+      ];
+      [renderCommandEncoder endEncoding];
+      [commandBuffer presentDrawable:drawable];
+      [commandBuffer commit];
+    }
 
     [window makeKeyAndOrderFront:nil];
     [app run];
